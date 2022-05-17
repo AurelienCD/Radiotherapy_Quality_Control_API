ce40d13433b29863944fd221f1e9aca3a710ea4c
AurelienCD <a.corroyer-dulmont@baclesse.unicancer.fr>
2022-05-17 15:44:34 +0200

    Update API_CQ_patient_predictor.py

--- API_CQ_patient_predictor.py
+++ API_CQ_patient_predictor.py
@@ -23,7 +23,7 @@ def main():
     indices = post
     label = "Sélectionner la localisation tumorale"
     options = ["Générale", "Pelvis", "Sein", "ORL", "Crâne", "Thorax"]
-    st.radio(label, options, index=0, format_func=special_internal_function, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False)
+    st.radio(label, options, index=0, format_func=special_internal_function, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
     
     try:
         ## Préparation des données
