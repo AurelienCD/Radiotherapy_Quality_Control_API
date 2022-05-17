5c490185cb799b6529125842b69b16d2548210f3
AurelienCD <a.corroyer-dulmont@baclesse.unicancer.fr>
2022-05-17 14:16:37 +0200

    Update API_CQ_patient_predictor.py

--- API_CQ_patient_predictor.py
+++ API_CQ_patient_predictor.py
@@ -55,7 +55,7 @@ def main():
             result = np.where(y_pred_prob[:,0]>0.30,0,1)
             predictions = result[0]
             if predictions == 1:
-                CQ_result = '\033[1m' + 'Conforme' + '\033[0m'
+                CQ_result = ('\033[1m' + 'Conforme' + '\033[0m')
             elif predictions == 0:
                 CQ_result = "Non-conforme"
             else:
