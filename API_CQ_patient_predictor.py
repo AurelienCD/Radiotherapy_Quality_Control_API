011dda56997a86c8039a5100c9221a030ec3ffc2
AurelienCD <a.corroyer-dulmont@baclesse.unicancer.fr>
2022-05-17 14:30:38 +0200

    Update API_CQ_patient_predictor.py

--- API_CQ_patient_predictor.py
+++ API_CQ_patient_predictor.py
@@ -83,7 +83,7 @@ def main():
 
             ## machine_learning_classification ##
             st.write('Pour le modèle de machine learning classification (RandomForestClassifier) : \n')
-            st.write('\033[92m' + 'Le résultat du CQ est : ' + machine_learning_classification(indices)  + '\033[0m')          
+            st.write('Le résultat du CQ est : ' + machine_learning_classification(indices))          
             st.image(image_ML, caption='ROC curve and confusion matrice for the RandomForestClassifier')
         
     except Exception as e:
