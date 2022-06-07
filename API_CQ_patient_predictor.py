3a04b643887732d6515c5b7ccb6d08b5de29c7b8
AurelienCD <a.corroyer-dulmont@baclesse.unicancer.fr>
2022-06-07 16:17:51 +0200

    Update API_CQ_patient_predictor.py

--- API_CQ_patient_predictor.py
+++ API_CQ_patient_predictor.py
@@ -110,9 +110,9 @@ def main():
             pred = None
                  
             my_bar = st.progress(0)
-            for percent_complete in range(10):
+            for percent_complete in range(100):
                  time.sleep(0.1)
-                 my_bar.progress(percent_complete + 1)
+                 my_bar.progress(percent_complete + 10)
             
             ## machine_learning_classification ##
             st.write('Pour le modèle de Machine Learning : \n')    
