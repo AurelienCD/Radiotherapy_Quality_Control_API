84fe5fe407c11c96ba494c7e79292e49232c7164
AurelienCD <a.corroyer-dulmont@baclesse.unicancer.fr>
2022-06-07 16:26:40 +0200

    Update API_CQ_patient_predictor.py

--- API_CQ_patient_predictor.py
+++ API_CQ_patient_predictor.py
@@ -111,7 +111,7 @@ def main():
             st.empty()    
             my_bar = st.progress(0)
             for percent_complete in range(100):
-                 time.sleep(0.01)
+                 time.sleep(0.05)
                  my_bar.progress(percent_complete + 1)
             
             ## machine_learning_classification ##
